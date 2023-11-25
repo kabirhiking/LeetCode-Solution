1be20e36d72f648e594810a79dd981dbc9dbfa22
kabir <86849015+5kabir@users.noreply.github.com>
2023-11-26 02:24:06 +0600

    first commit

---
+++ C++/412_Fizz_Bizz.cpp
@@ -0,0 +1,31 @@
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        
+    vector<string> answer;
+      
+         
+        for(int i=1; i<=n; i++){
+            if(i%3 == 0 && i%5 == 0){
+                answer.push_back("FizzBuzz");
+            }
+            else if(i%3 == 0){
+                answer.push_back("Fizz");
+            }
+            else if(i%5 == 0){
+                answer.push_back("Buzz");
+            }
+            else{
+                answer.push_back(to_string(i)); 
+            }
+        
+        } 
+        return answer;
+    }
+
+};
+
+// 1.use example to visualize the solution 
+// 2.english instruction
+// 3.coding
